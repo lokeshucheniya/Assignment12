0e98eded3e148b6d3612e9d222d3fd1ba5844aa8
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 23:20:15 +0530

    Update TrainingTest.java

--- TrainingTest.java
+++ TrainingTest.java
@@ -1,10 +1,14 @@
-abstract public class Training {
 
-	Training(String name,int fees)
-	{
+//Interface Training 
+abstract public class Training {
+	
+	//Parameterised Constructor
+	Training(String name,int fees) {
 		this.fees=fees;
 		subject=name;
 	}
+	
+	//Instance Variable
 	private int id;
 	private String subject;
 	public int fees;
@@ -13,48 +17,57 @@ abstract public class Training {
 	
 }
 
-public class PublicTraining extends Training{
+//Public Traing Class extends Training
+public class PublicTraining extends Training {
 
-	 public PublicTraining(String name,int fees, int participants)
-	{
+	//Parameterised Constructor
+	 public PublicTraining(String name,int fees, int participants) {
 		super(name,fees);
 		this.name=name;
 		this.participants=participants;
 		
 	}
-	 private String name;
+	
+	private String name;
 	private int participants;
 	private int cost;
-	public int getOrderValue()
-	{
+	
+	
+	//Method getOrderValue for returning cost
+	public int getOrderValue() {
 		this.cost=participants*fees;
 		return cost;
 	}
-	public void disp()
-	{
+	
+	//Display Method  for displaying course
+	public void disp() {
 		System.out.println("Training cost for "+name+" course is "+getOrderValue());
 	}
 }
 
-public class CorporateTraining extends Training{
+//Class Corporate Training inherits Training
+public class CorporateTraining extends Training {
 	
-	public CorporateTraining(String name,int fees, int days)
-	{
+	//Parameterised Constructor
+	public CorporateTraining(String name,int fees, int days){
 		 super(name,fees);
 		this.name=name;
 		this.days=days;
 		
-	}
+	} 
+	
 	private int days;
 	private int cost;
 	private String name;
-	public int getOrderValue()
-	{
+	
+	//Method getOrderValue for returning cost
+	public int getOrderValue() {
 		this.cost=days*fees;
 		return cost;
 	}
-	public void disp()
-	{
+	
+	//Display Method  for displaying course
+	public void disp() {
 		System.out.println("Training cost for "+name+" course is "+getOrderValue());
 	}
 
@@ -63,12 +76,11 @@ public class CorporateTraining extends Training{
 
 public class TrainingTest {
  
-	public static void main(String args[])
-	{
-		Training publicTraining=new PublicTraining("Java",5000,50);
+	public static void main(String args[]) {
+		Training publicTraining=new PublicTraining("Java",5000,50); 		//Passing values to Class Public Training
 		publicTraining.disp();
 		
-		Training corporateTraining=new CorporateTraining("Big Data",35000,4);
+		Training corporateTraining=new CorporateTraining("Big Data",35000,4);	//Passing values to Class Corporate Training
 		corporateTraining.disp();
 		
 	}
